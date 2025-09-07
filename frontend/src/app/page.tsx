acf595f41f5393a699e74f388029edae6ef8728b
makidonk <mathilda.magnusson@gmail.com>
2025-09-07 19:37:31 +0200

    started animations for hero

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,36 +6,34 @@ export default function HomePage() {
       <main className="min-h-screen bg-background">
         <SplashScreen />
         <div className="px-[20px] flex flex-col justify-center align-center">
-          <p className="text-[4vw] xl:text-[2vw] text-black  font-playfair pl-[40px] lg:pl-[130px] md:pl-[90px] leading-none">
+          <p className="text-[4vw] xl:text-[2vw] text-black  font-playfair pl-[40px] lg:pl-[130px] md:pl-[90px] leading-none z-2">
             hi, my name is
           </p>
-          <p className=" text-black font-extrabold font-anton text-[23vw] xl:text-[19vw] text-center xl:text-left leading-none">
+          <p className=" text-black font-extrabold font-anton text-[23vw] xl:text-[19vw] text-center xl:text-left leading-none z-2">
             MATHILDA
           </p>
-          <p className="text-[4vw] xl:text-[2vw] text-black  font-playfair pl-[20px] sm:pl-[50px] leading-none">
+          <p className="text-[4vw] xl:text-[2vw] text-black  font-playfair pl-[20px] sm:pl-[50px] leading-none z-2">
             and I’m a
           </p>
-          <p className="text-[10vw] xl:text-[7vw] text-black font-extrabold pl-[60px] sm:pl-[90px] md:pl-[130px] font-anton">
+          <p className="text-[10vw] xl:text-[7vw] text-black font-extrabold pl-[60px] sm:pl-[90px] md:pl-[130px] font-anton z-2">
             WEB DEVELOPER
           </p>
-          <div className="hidden absolute right-[5vw] top-[80px] h-[100vh] xl:flex flex-row gap-20">
-            <div>
-              <img
-                src="./flower.svg"
-                alt="Mathilda Logo"
-                className={`  object-contain  scale-x-[-1] h-[40px]`}
-              />
-              <img
-                src="./flower.svg"
-                alt="Mathilda Logo"
-                className={`  object-contain  scale-x-[-1] h-[40px]`}
-              />
-              <img
-                src="./flower.svg"
-                alt="Mathilda Logo"
-                className={`  object-contain  scale-x-[-1] h-[40px]`}
-              />
-              <button className="self-end px-[30px] py-[10px] bg-[#e63f3f] hover:bg-[#A02C2C]  mb-[25vh] font-anton text-[1.5vw] text-[#E5AEAE]">
+          <div className="hidden absolute right-[5vw] top-[80px] h-[100vh] xl:flex flex-row gap-0 z-1">
+            <div className="flex flex-col gap-10 justify-end">
+              <div className="flex flex-row">
+                <img
+                  src="./flower.svg"
+                  alt="Mathilda Logo"
+                  className={`self-end object-contain  scale-x-[-1] h-[6vw] hover:-rotate-60 rotate-0  transition-all duration-500`}
+                />
+                <img
+                  src="./flower.svg"
+                  alt="Mathilda Logo"
+                  className={`object-contain  scale-x-[-1] h-[15vw] mb-12 hover:-rotate-40 rotate-15 transition-all duration-500`}
+                />
+              </div>
+
+              <button className="self-end px-[50px] py-[20px] bg-[#e63f3f] hover:-rotate-15 rotate-15 transition-all duration-500 cursor-pointer mb-[5vh] font-anton text-[2vw] text-[#E5AEAE]  ml-[100px]">
                 CONTACT ME
               </button>
             </div>
@@ -46,18 +44,18 @@ export default function HomePage() {
             />
           </div>
 
-          <p className=" text-black pl-[20px] sm:px-[50px]  font-playfair xl:w-[50vw] md:text-2xl text-md pb-3">
+          <p className=" text-black pl-[20px] sm:px-[50px]  font-playfair xl:w-[50vw] md:text-2xl text-md pb-3 z-2">
             I have a background in web dev studies and hands-on experience as a
             frontend developer. I build modern web apps using React, TypeScript,
             Tailwind CSS, and Node.js, and I’m always curious and excited to
             learn new technologies.
           </p>
-          <p className=" text-black pl-[20px] sm:px-[50px] font-playfair xl:w-[50vw] md:text-2xl text-md pb-3">
+          <p className=" text-black pl-[20px] sm:px-[50px] font-playfair xl:w-[50vw] md:text-2xl text-md pb-3 z-2">
             Right now, I’m actively looking for a full-time position where I can
             grow as a developer, contribute to a team, and keep building great
             things.
           </p>
-          <p className=" text-black pl-[20px] sm:px-[50px] font-playfair xl:w-[50vw] md:text-2xl text-md">
+          <p className=" text-black pl-[20px] sm:px-[50px] font-playfair xl:w-[50vw] md:text-2xl text-md z-2">
             This portfolio is a work in progress - just like my journey as a
             developer :)
           </p>
